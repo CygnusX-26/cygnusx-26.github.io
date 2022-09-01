571b45186aac9035690da69cf2988527a0a27feb
CygnusX-26 <qh4nveh@gmail.com>
2022-09-01 16:40:31 -0400

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="h-screen w-screen">
+        <div class="h-screen w-screen flex flex-col justify-center items-center" id="app">
+          <div class = "main-text" @click="onDivClicked">
+            <p class="text-4xl font-bold text-gray-800" unselectable="on"> {{ message }} </p>
+            <br>
+            <div class="font-bold text-emerald-700">WIP, will be completed later</div>
+          </div>
+            
+        </div>
+    </div>
+    
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        toggle: true,
+        message: "Hello World"
+      }
+    },
+    methods: {
+      onDivClicked: function () {
+        this.toggle = !this.toggle
+        if (this.toggle){
+          this.message = "Hello World"
+        }
+        else{
+          this.message = "Goodbye World"
+        }
+      }
+    }
+  }
+  </script>
+
