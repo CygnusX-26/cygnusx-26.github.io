fa3a37b174b79d69541bd09f1f4627ea62807b0f
CygnusX-26 <qh4nveh@gmail.com>
2022-09-02 00:51:57 -0400

    Update pgae logic

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
     <div class="h-screen w-screen">
         <div class="h-screen w-screen flex flex-col justify-center items-center" id="app">
           <div class = "main-text" @click="onDivClicked">
-            <p class="text-4xl font-bold text-gray-800" unselectable="on"> {{ message }} </p>
+            <p class="text-4xl font-bold text-gray-800" unselectable="on"> {{ title }} </p>
             <br>
-            <div class="font-bold text-emerald-700">WIP, will be completed later</div>
+            <div class="font-bold text-emerald-700">{{ message }}</div>
           </div>
             
         </div>
@@ -15,19 +15,33 @@
   export default {
     data() {
       return {
-        toggle: true,
-        message: "Hello World"
+        counter: 0,
+        list:  [
+			"Introduction", 
+			"About Me", 
+			"Projects", 
+			"Contact Me"
+		],
+		bodyList: [
+			"Hey, I'm Neil, an 18 year old developer from California. I am currently studying Computer Science at Purdue University",
+			"I'm a full stack developer, and I'm currently working on a few projects.",
+			"Here are some of my projects:",
+			"Contact me at"
+		],
+		title: "Introduction",
+        message: "Hey, I'm Neil, an 18 year old developer from California. I am currently studying Computer Science at Purdue University"
       }
     },
     methods: {
       onDivClicked: function () {
-        this.toggle = !this.toggle
-        if (this.toggle){
-          this.message = "Hello World"
+        if (this.counter < this.list.length - 1) {
+          this.counter += 1
         }
         else{
-          this.message = "Goodbye World"
+          this.counter = 0
         }
+		this.title = this.list[this.counter]
+        this.message = this.bodyList[this.counter]
       }
     }
   }
