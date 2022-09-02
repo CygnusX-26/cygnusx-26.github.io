77f3cc592b61d58edc47df7a999e6ccbd8bec8fb
CygnusX-26 <qh4nveh@gmail.com>
2022-09-02 11:50:52 -0400

    Website content

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,52 @@
 <template>
-    <div class="h-screen w-screen">
+    <div class="h-screen w-screen" @click="onDivClicked">
         <div class="h-screen w-screen flex flex-col justify-center items-center" id="app">
-          <div class = "main-text" @click="onDivClicked">
-            <p class="text-4xl font-bold text-gray-800" unselectable="on"> {{ title }} </p>
-            <br>
-            <div class="font-bold text-emerald-700">{{ message }}</div>
-          </div>
-            
+			<Transition name="slide-fade">
+				<div class = "main-text" v-if="show">
+					<p class="text-4xl font-bold text-gray-800"> {{ title }} </p>
+					<br>
+					<p class="font-bold text-emerald-700">{{ message }}</p>
+					<p class="font-bold text-emerald-800" v-if="counter==2" href="https://github.com/CygnusX-26">Github page</p>
+					<p class="font-bold text-emerald-800" v-if="counter==3" >Discord: CygnusX#5667</p>
+					<p class="font-bold text-emerald-800" v-if="counter==3">Email: qh4nveh@gmail.com</p>
+					<a class="font-bold text-emerald-800" v-if="counter==3" href="https://www.linkedin.com/in/neil-van-eikema-hommes-34b369218/">Linked In</a>
+				</div>
+			</Transition>
         </div>
     </div>
-    
 </template>
 <script>
   export default {
     data() {
       return {
         counter: 0,
+		show: true,
         list:  [
 			"Introduction", 
 			"About Me", 
 			"Projects", 
-			"Contact Me"
+			"Get in touch",
 		],
 		bodyList: [
 			"Hey, I'm Neil, an 18 year old developer from California. I am currently studying Computer Science at Purdue University",
 			"I'm a full stack developer, and I'm currently working on a few projects.",
-			"Here are some of my projects:",
-			"Contact me at"
+			"You can see what I'm working on by visiting my",
+			"Contact me at",
 		],
-		title: "Introduction",
-        message: "Hey, I'm Neil, an 18 year old developer from California. I am currently studying Computer Science at Purdue University"
+		title: "I'm Neil",
+        message: "an 18 year old developer from California \ncurrently studying Computer Science at Purdue University.",
       }
     },
     methods: {
       onDivClicked: function () {
-        if (this.counter < this.list.length - 1) {
-          this.counter += 1
+		this.show = !this.show;
+        if (!this.show) {
+          	this.counter ++
         }
         else{
-          this.counter = 0
+			if (this.counter > this.list.length - 1) {
+          		this.counter = 0
+			}
         }
 		this.title = this.list[this.counter]
         this.message = this.bodyList[this.counter]
